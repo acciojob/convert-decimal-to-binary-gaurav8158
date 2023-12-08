b446b0b27f3abda311c2a5ddd22be18021835720
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-12-08 23:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+// function decimalToBinary(num) {
+//   // your code here
+// 	if(num===0)  return 0;
+// let sum = "";
+// while(num>0){
+// let rem=num%2;
+// sum=rem+sum;
+// num=Math.floor(num/2);
+// }
+// return sum;
+// } 
+// const num = parseInt(prompt("Enter a number."));
+// alert(decimalToBinary(num));
+
+
 function decimalToBinary(num) {
-  // your code here
-	if(num===0)  return 0;
-let sum = "";
-while(num>0){
-let rem=num%2;
-sum=rem+sum;
-num=Math.floor(num/2);
+  // your code 
+	if (num === 0) {
+		return "0";
+	}
+	
+    let binaryStr = "";
+	while (num > 0) {
+		binaryStr = (num % 2) + binaryStr;
+        num = Math.floor(num / 2);
+    }
+
+    return binaryStr;
+	
 }
-return sum;
-} 
 const num = parseInt(prompt("Enter a number."));
 alert(decimalToBinary(num));
